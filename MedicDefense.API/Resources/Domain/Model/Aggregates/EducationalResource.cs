ccb9ced201b7e854e8f29050576b935753de0fc0
Gabotw <u202018627@upc.edu.pe>
2024-06-06 13:28:12 -0500

    feat: added educational resource aggregate.

---
+++ MedicDefense.API/Resources/Domain/Model/Aggregates/EducationalResource.cs
@@ -0,0 +1,29 @@
+﻿using MedicDefense.API.Resources.Domain.Model.Commands;
+
+namespace MedicDefense.API.Resources.Domain.Model.Aggregates;
+
+public class EducationalResource
+{
+    public int Id { get; private set; }
+    
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public string ContentType { get; private set; }
+    public string VideoUrl { get; private set; }
+    
+    protected EducationalResource()
+    {
+        this.Title = string.Empty;
+        this.Author = string.Empty;
+        this.ContentType = string.Empty;
+        this.VideoUrl = string.Empty;
+    }
+
+    public EducationalResource(CreateEducationalResourceCommand command)
+    {
+        this.Title = command.Title;
+        this.Author = command.Author;
+        this.ContentType = command.ContentType;
+        this.VideoUrl = command.VideoUrl;
+    }
+}
---
+++ MedicDefense.API/Resources/Domain/Model/Commands/CreateEducationalResourceCommand.cs
@@ -0,0 +1,3 @@
+﻿namespace MedicDefense.API.Resources.Domain.Model.Commands;
+
+public record CreateEducationalResourceCommand(string Title, string Author, string ContentType, string VideoUrl);
