e0c17e744168d4b0cf3842cb3aca4fc7d4373b44
Gabotw <u202018627@upc.edu.pe>
2024-06-06 13:43:42 -0500

    ci: update and delete unnecessary code.

--- MedicDefense.API/Program.cs
+++ MedicDefense.API/Program.cs
@@ -1,14 +1,64 @@
+using MedicDefense.API.Resources.Application.Internal.CommandServices;
+using MedicDefense.API.Resources.Application.Internal.QueryServices;
+using MedicDefense.API.Resources.Domain.Repositories;
+using MedicDefense.API.Resources.Domain.Services;
+using MedicDefense.API.Resources.Infrastructure.Persistence.EFC.Repositories;
+using MedicDefense.API.Shared.Domain.Repositories;
+using MedicDefense.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using MedicDefense.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using MedicDefense.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(
+    options =>
+    {
+        options.Conventions.Add(new KebabCaseRouteNamingConvention());   
+    });
+// Database Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+    });
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Resources Bounded Context Injection Configuration
+builder.Services.AddScoped<IEducationalResourceCommandService, EducationalResourceCommandService>();
+builder.Services.AddScoped<IEducationalResourceQueryService, EducationalResourceQueryService>();
+builder.Services.AddScoped<IEducationalResourceRepository, EducationalResourceRepository>();
 var app = builder.Build();
 
+// Verify Database objects are created
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
