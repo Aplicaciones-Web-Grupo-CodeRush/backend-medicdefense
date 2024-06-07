981eba4716856a2a3917d60d291d011176284c6c
Fabricio Apaza Morales <josueapaza01@gmail.com>
2024-06-07 15:30:56 -0500

    feat(legalcase): add LegalCase aggregate and structure

---
+++ MedicDefense.API/LegalCase/Domain/Model/Aggregates/LegalCase.cs
@@ -0,0 +1,30 @@
+﻿using MedicDefense.API.LegalCase.Domain.Model.Commands;
+
+namespace MedicDefense.API.LegalCase.Domain.Model;
+
+public class LegalCase
+{
+    public int Id { get; private set; }
+    
+    public string CaseNumber { get; private set; }
+    
+    public string Description { get; private set; }
+    
+    public string Status { get; private set; }
+
+    protected LegalCase()
+    {
+        this.CaseNumber = string.Empty;
+        this.Description = string.Empty;
+        this.Status = string.Empty;
+    }
+
+    public LegalCase(CreateLegalCaseCommand command)
+    {
+        this.CaseNumber = command.CaseNumber;
+        this.Description = command.Description;
+        this.Status = command.Status;
+    }
+    
+    
+}
