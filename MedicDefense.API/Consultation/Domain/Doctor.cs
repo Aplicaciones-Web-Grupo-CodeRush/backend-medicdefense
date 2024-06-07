deacd44ca3c30186e2625d9609d0d180e3ea4543
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-06-07 17:02:40 -0500

    feat(consultation): added domain

---
+++ MedicDefense.API/Consultation/Domain/Doctor.cs
@@ -0,0 +1,8 @@
+﻿namespace MedicDefense.API.Consultation.Domain;
+
+public class Doctor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+}
