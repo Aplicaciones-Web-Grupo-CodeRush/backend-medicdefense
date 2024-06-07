1ce5e12039a2796384f2b0377b5123fe1392e604
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-06-07 17:07:58 -0500

    feat(consultation): update files

--- MedicDefense.API/Program.cs
+++ MedicDefense.API/Program.cs
@@ -1,6 +1,16 @@
+using MedicDefense.API.Consultation.Application.Interfaces;
+using MedicDefense.API.Consultation.Application.Services;
+using MedicDefense.API.Consultation.Infrastructure.Repositories;
+using MedicDefense.API.Consultation.Interfaces;
+using MedicDefense.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IConsultRepository, ConsultRepository>(); 
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>(); 
+builder.Services.AddScoped<ILawyerRepository, LawyerRepository>(); 
+builder.Services.AddScoped<IConsultationService, ConsultationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
