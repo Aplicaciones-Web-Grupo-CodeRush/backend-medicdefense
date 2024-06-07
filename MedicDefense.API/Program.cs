c778f554eadbd64766c727e6a1739ed64a797f26
MarceloRenteriaUPC <149633136+MarceloRenteriaUPC@users.noreply.github.com>
2024-06-07 17:57:39 -0500

    feat: Updated the resources found on Shared or the general project to include the newly added Payment Bounded Context.

--- MedicDefense.API/Program.cs
+++ MedicDefense.API/Program.cs
@@ -1,13 +1,71 @@
+using MedicDefense.API.Payment.Application.Internal.CommandServices;
+using MedicDefense.API.Payment.Application.Internal.QueryServices;
+using MedicDefense.API.Payment.Domain.Repositories;
+using MedicDefense.API.Payment.Domain.Services;
+using MedicDefense.API.Payment.Infrastructure.Persistence.EFC.Repositories;
+using MedicDefense.API.Shared.Domain.Repositories;
+using MedicDefense.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using MedicDefense.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using MedicDefense.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(
+    options =>
+    {
+        options.Conventions.Add(new KebabCaseRouteNamingConvention());   
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Database Context and Logging Levels
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+    });
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Shared Bounded Context Injection Configuration
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Resources Bounded Context Injection Configuration
+builder.Services.AddScoped<IPaymentInfoCommandService, PaymentInfoCommandService>();
+builder.Services.AddScoped<IPaymentInfoQueryService, PaymentInfoQueryService>();
+builder.Services.AddScoped<IPaymentInfoRepository, PaymentInfoRepository>();
+builder.Services.AddScoped<IPriceCommandService, PriceCommandService>();
+builder.Services.AddScoped<IPriceQueryService, PriceQueryService>();
+builder.Services.AddScoped<IPriceRepository, PriceRepository>();
+builder.Services.AddScoped<ICardInfoCommandService, CardInfoCommandService>();
+builder.Services.AddScoped<ICardInfoQueryService, CardInfoQueryService>();
+builder.Services.AddScoped<ICardInfoRepository, CardInfoRepository>();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
