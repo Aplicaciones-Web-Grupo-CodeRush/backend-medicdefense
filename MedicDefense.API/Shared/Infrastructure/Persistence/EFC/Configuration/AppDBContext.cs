52ce56f860cb9738f278259c4fa1f4b041becab2
Gabotw <129527889+Gabotw@users.noreply.github.com>
2024-06-08 01:43:54 -0500

    Revert "Marcelo renteria"

--- MedicDefense.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDBContext.cs
+++ MedicDefense.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDBContext.cs
@@ -32,40 +32,6 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
                 n.Property(i => i.Information).HasColumnName("Information");
             });
 
-        // Place here your entities configuration
-        builder.Entity<PaymentInfo>().ToTable("payments");
-        builder.Entity<PaymentInfo>().HasKey(p => p.Id);
-        builder.Entity<PaymentInfo>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<PaymentInfo>().Property(l => l.Description).IsRequired();
-        
-        builder.Entity<Price>().ToTable("prices");
-        builder.Entity<Price>().HasKey(p => p.Id);
-        builder.Entity<Price>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<Price>().Property(p => p.Amount).IsRequired();
-        builder.Entity<Price>().Property(p => p.Type).IsRequired();
-        builder.Entity<Price>().Property(p => p.MedicId).IsRequired();
-        builder.Entity<Price>().Property(p => p.LawyerId).IsRequired();
-        
-        builder.Entity<CardInfo>().ToTable("cards");
-        builder.Entity<CardInfo>().HasKey(c => c.Id);
-        builder.Entity<CardInfo>().Property(c => c.Id).IsRequired().ValueGeneratedOnAdd();
-        builder.Entity<CardInfo>().Property(c => c.CardNumber).IsRequired();
-        builder.Entity<CardInfo>().Property(c => c.SecurityNumber).IsRequired();
-        
-        // Price Relationships
-        builder.Entity<Price>()
-            .HasMany(p => p.PaymentInfos)
-            .WithOne(i => i.Price)
-            .HasForeignKey(i => i.PriceId)
-            .HasPrincipalKey(p => p.Id);
-        
-        builder.Entity<CardInfo>()
-            .HasMany(c => c.PaymentInfos)
-            .WithOne(p => p.CardInfo)
-            .HasForeignKey(i => i.CardInfoId)
-            .HasPrincipalKey(p => p.Id);
-
-
         builder.Entity<LegalCase.Domain.Model.LegalCase>().ToTable("LegalCases");
         builder.Entity<LegalCase.Domain.Model.LegalCase>().HasKey(l => l.Id);
         builder.Entity<LegalCase.Domain.Model.LegalCase>().Property(l => l.Id).IsRequired().ValueGeneratedOnAdd();
