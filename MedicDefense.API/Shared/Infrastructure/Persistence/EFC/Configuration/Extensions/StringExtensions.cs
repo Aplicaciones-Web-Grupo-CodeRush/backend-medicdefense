6fbe5040ab6af9470a6df9d9fb01dba245333cb6
MarceloRenteriaUPC <149633136+MarceloRenteriaUPC@users.noreply.github.com>
2024-06-08 01:34:17 -0500

    chore: Added a }.

--- MedicDefense.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
+++ MedicDefense.API/Shared/Infrastructure/Persistence/EFC/Configuration/Extensions/StringExtensions.cs
@@ -28,6 +28,7 @@ public static class StringExtensions
     public static T ToEnum<T>(this string value)
     {
         return (T) Enum.Parse(typeof(T), value, true);
+    }
 
 
     public static string ToPlural(this string text)
