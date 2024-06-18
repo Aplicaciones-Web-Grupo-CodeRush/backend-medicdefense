b3b686b07a880fa4a8d641af9c57d6240fad670b
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-06-17 19:08:29 -0500

    feat(consultation): update consult controller

--- MedicDefense.API/Consultation/Infrastructure/Controllers/ConsultationController.cs
+++ MedicDefense.API/Consultation/Infrastructure/Controllers/ConsultationController.cs
@@ -1,41 +1,73 @@
-﻿using MedicDefense.API.Consultation.Application.Interfaces;
-using MedicDefense.API.Consultation.Application.Services;
-using MedicDefense.API.Consultation.Domain;
+﻿using System.Net.Mime;
+using MedicDefense.API.Consultation.Application.CommandServices;
+using MedicDefense.API.Consultation.Application.QueryServices;
+using MedicDefense.API.Consultation.Domain.Model.Aggregates;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using MedicDefense.API.Consultation.Domain.Model.Queries;
+using MedicDefense.API.Consultation.Domain.Services;
+using MedicDefense.API.Consultation.Interfaces.REST.Resources;
+using MedicDefense.API.Consultation.Interfaces.REST.Transform;
 
 namespace MedicDefense.API.Consultation.Infrastructure.Controllers;
 
-
 [ApiController]
 [Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+
 public class ConsultationController : ControllerBase
 {
-    private readonly IConsultationService _consultationService;
+    private readonly IConsultCommandService _consultCommandService;
+    private readonly IConsultQueryService _consultQueryService;
 
-    public ConsultationController(IConsultationService consultationService)
+    public ConsultationController(IConsultCommandService consultCommandService, IConsultQueryService consultQueryService)
     {
-        _consultationService = consultationService;
+        _consultCommandService = consultCommandService;
+        _consultQueryService = consultQueryService;
     }
 
     // GET: api/Consultation/{id}
     [HttpGet("{id}")]
-    public ActionResult<Consult> GetConsult(int id)
+    public async Task<ActionResult<ConsultRes>> GetConsult(int id)
     {
-        var consult = _consultationService.GetConsult(id);
+        var consult = await _consultQueryService.Handle(new GetConsultByIdQuery(id));
         if (consult == null)
         {
             return NotFound();
         }
-        return consult;
+        var consultRes = ConsultResFromEntityAssembler.ToResourceFromEntity(consult);
+        return consultRes;
     }
 
     // POST: api/Consultation
     [HttpPost]
-    public ActionResult<Consult> CreateConsult([FromBody] Consult consult)
+    public async Task<IActionResult> CreateConsult([FromBody] CreateConsultRes consultRes)
     {
-        _consultationService.CreateConsult(consult);
-        return CreatedAtAction(nameof(GetConsult), new { id = consult.Id }, consult);
+        var createConsultCommand = CreateConsultCommandFromResAssembler.ToCommandFromResource(consultRes);
+        var consult = await _consultCommandService.HandleCreateConsultCommand(createConsultCommand);
+        return CreatedAtAction(nameof(GetConsult), new { id = consult.Id }, consultRes);
     }
+    
+    //POST: api/Doctor
+    [HttpPost("Doctor")]
+    public async Task<IActionResult> CreateDoctor([FromBody] DoctorRes doctorRes)
+    {
+        var createDoctorCommand = CreateDoctorCommandFromResAssembler.ToCommandFromResource(doctorRes);
+        var doctor = await _consultCommandService.HandleCreateDoctorCommand(createDoctorCommand);
+        return CreatedAtAction(nameof(GetConsult), new { id = doctor.Id }, doctorRes);
+    }
+    
+    //POST: api/Lawyer
+    [HttpPost("Lawyer")]
+    public async Task<IActionResult> CreateLawyer([FromBody] LawyerRes lawyerRes)
+    {
+        var createLawyerCommand = CreateLawyerCommandFromResAssembler.ToCommandFromResource(lawyerRes);
+        var lawyer = await _consultCommandService.HandleCreateLawyerCommand(createLawyerCommand);
+        return CreatedAtAction(nameof(GetConsult), new { id = lawyer.Id }, lawyerRes);
+    }
+    
+    
+ 
 
     // PUT: api/Consultation/{id}
     [HttpPut("{id}")]
@@ -45,7 +77,7 @@ public class ConsultationController : ControllerBase
         {
             return BadRequest();
         }
-        _consultationService.UpdateConsult(consult);
+        _consultCommandService.UpdateConsult(consult);
         return NoContent();
     }
 
@@ -53,7 +85,9 @@ public class ConsultationController : ControllerBase
     [HttpDelete("{id}")]
     public IActionResult DeleteConsult(int id)
     {
-        _consultationService.DeleteConsult(id);
+        _consultCommandService.DeleteConsult(id);
         return NoContent();
     }
+    
+    
 }
