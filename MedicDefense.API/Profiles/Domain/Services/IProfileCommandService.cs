b593015d997daefbdb6eb3ca91f563605150e6cd
CesarrAnn <129460134+CesarrAnn@users.noreply.github.com>
2024-06-23 22:20:05 -0500

    feat(profile): Added Interface Command and Query Service

---
+++ MedicDefense.API/Profiles/Domain/Services/IProfileCommandService.cs
@@ -0,0 +1,10 @@
+using MedicDefense.API.Profiles.Domain.Model.Aggregates;
+using MedicDefense.API.Profiles.Domain.Model.Commands;
+
+namespace MedicDefense.API.Profiles.Domain.Services;
+
+public interface IProfileCommandService
+{
+    Task<Profile> HandleCreateProfileCommand(CreateProfileCommand command);
+    Task DeleteProfile(int id);
+}
